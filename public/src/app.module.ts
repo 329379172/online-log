d527f9242d716b594c925e53c10df1cbf4d00f63
林飞扬 <329379172@qq.com>
2016-10-26 21:00:35 +0800

    first commit

---
+++ public/src/app.module.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by linfeiyang on 16-10-26.
+ */
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+
+@NgModule({
+	imports: [BrowserModule]
+})
+export class AppModule {
+	
+}
